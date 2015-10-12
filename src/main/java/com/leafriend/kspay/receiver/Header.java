4dd3cbe64e0520f79556b5327835a69bce9e24dd
leafriend <leafriend@gmail.com>
2015-10-12 18:13:12 +0900

    전문 헤더 구체 클래스의 필드명 앞에 언더바(_)를 붙임
    KSNET에서 지정한 이름을 최대한 유지하면서 다른 이름과 충돌을 피하기 위해.

--- src/main/java/com/leafriend/kspay/receiver/Header.java
+++ src/main/java/com/leafriend/kspay/receiver/Header.java
@@ -7,113 +7,113 @@ public class Header {
     /**
      * 길이
      */
-    private String RecvLen;
+    private String _RecvLen;
 
     /**
      * 0: 암화안함, 1:openssl, 2: seed
      */
-    private String EncType;
+    private String _EncType;
 
     /**
      * 전문버전
      */
-    private String Version;
+    private String _Version;
 
     /**
      * 구분
      */
-    private String VersionType;
+    private String _VersionType;
 
     /**
      * 전송구분 : 0 : 처음, 2: 재전송
      */
-    private String Resend;
+    private String _Resend;
 
     /**
      * 요청일자 : yyyymmddhhmmss
      */
-    private String RequestDate;
+    private String _RequestDate;
 
     /**
      * 상점아이디
      */
-    private String StoreId;
+    private String _StoreId;
 
     /**
      * 주문번호
      */
-    private String OrderNumber;
+    private String _OrderNumber;
 
     /**
      * 주문자명
      */
-    private String UserName;
+    private String _UserName;
 
     /**
      * 주민번호 or 사업자번호
      */
-    private String IdNum;
+    private String _IdNum;
 
     /**
      * email
      */
-    private String Email;
+    private String _Email;
 
     /**
      * 제품구분 0 : 실물, 1 : 디지털
      */
-    private String GoodType;
+    private String _GoodType;
 
     /**
      * 제품명
      */
-    private String GoodName;
+    private String _GoodName;
 
     /**
      * KeyInType 여부 : 1 : Swap, 2: KeyIn
      */
-    private String KeyInType;
+    private String _KeyInType;
 
     /**
      * lineType 0 : offline, 1:internet, 2:Mobile
      */
-    private String LineType;
+    private String _LineType;
 
     /**
      * 휴대폰번호
      */
-    private String PhoneNo;
+    private String _PhoneNo;
 
     /**
      * 승인갯수
      */
-    private String ApprovalCount;
+    private String _ApprovalCount;
 
     /**
      * 예비
      */
-    private String HeadFiller;
+    private String _HeadFiller;
 
     public static Header parse(MessageReader reader) throws EOFException, IOException {
         Header instance = new Header();
-        instance.RecvLen = reader.read(4);
-        instance.EncType = reader.read(1);
-        instance.Version = reader.read(4);
-        instance.VersionType = reader.read(2);
-        instance.Resend = reader.read(1);
-        instance.RequestDate = reader.read(14);
-        instance.StoreId = reader.read(10);
-        instance.OrderNumber = reader.read(50);
-        instance.UserName = reader.read(50);
-        instance.IdNum = reader.read(13);
-        instance.Email = reader.read(50);
-        instance.GoodType = reader.read(1);
-        instance.GoodName = reader.read(50);
-        instance.KeyInType = reader.read(1);
-        instance.LineType = reader.read(1);
-        instance.PhoneNo = reader.read(12);
-        instance.ApprovalCount = reader.read(1);
-        instance.HeadFiller = reader.read(35);
+        instance._RecvLen = reader.read(4);
+        instance._EncType = reader.read(1);
+        instance._Version = reader.read(4);
+        instance._VersionType = reader.read(2);
+        instance._Resend = reader.read(1);
+        instance._RequestDate = reader.read(14);
+        instance._StoreId = reader.read(10);
+        instance._OrderNumber = reader.read(50);
+        instance._UserName = reader.read(50);
+        instance._IdNum = reader.read(13);
+        instance._Email = reader.read(50);
+        instance._GoodType = reader.read(1);
+        instance._GoodName = reader.read(50);
+        instance._KeyInType = reader.read(1);
+        instance._LineType = reader.read(1);
+        instance._PhoneNo = reader.read(12);
+        instance._ApprovalCount = reader.read(1);
+        instance._HeadFiller = reader.read(35);
         return instance;
     }
 
